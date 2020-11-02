edee9fec28e3f5e4f068b4d48e21bc98ba3a74de
Sonja Friedl <72401504+Soleviso@users.noreply.github.com>
2020-11-02 16:59:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+user_name = input("Vorname: ")
+print("Hallo " + user_name + ", wie geht's dir heute?")
+
+mood = "gut drauf"
+
+user_mood = input(" Gefühlsstatus: ")
+
+if mood == "gut drauf":
+    print("sehr schön, weiter so!")
+else:
+    print("Es wird noch ein schöner Tag")
